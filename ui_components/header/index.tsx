8070fa799978738ed572eee66ff6d158f1bcb71a
Punith B M <punith@designstring.com>
2023-10-21 11:41:52 +0530

    fix:load time

--- ui_components/header/index.tsx
+++ ui_components/header/index.tsx
@@ -30,7 +30,10 @@ const Header = (props: IHeader) => {
   return (
     <header className="z-[9] fixed left-1/2 -translate-x-1/2 top-4 rounded-3xl h-[64px] bg-secondary-100 text-center flex items-center justify-between px-3 w-[calc(100vw-32px)] lg:w-[600px]">
       {step === 1 || step === 2 ? (
-        <Image src={icons.logo} alt="logo" className="w-10" />
+        <>
+          <Image src={icons.logo} alt="logo" className="w-10" />
+          <p className="heading3_bold text-white">{"Citizen"}</p>
+        </>
       ) : (
         <div className="flex items-center gap-3">
           <BackBtn
@@ -45,19 +48,11 @@ const Header = (props: IHeader) => {
       <div className="flex gap-4 items-center">
         <div className="relative">
           {isConnected ? (
-            <button
-              type="button"
-              onClick={signOut}
-              className="h-[40px] rounded-3xl bg-secondary-600 flex items-center px-3 py-2 justify-center gap-2"
-            >
+            <button type="button" onClick={signOut} className="h-[40px] rounded-3xl bg-secondary-600 flex items-center px-3 py-2 justify-center gap-2">
               <Image src={icons.logOut} alt="logout" className="w-6" />
             </button>
           ) : (
-            <button
-              type="button"
-              onClick={signIn}
-              className="h-[40px] rounded-3xl bg-secondary-600 flex items-center px-3 py-2 justify-center gap-2"
-            >
+            <button type="button" onClick={signIn} className="h-[40px] rounded-3xl bg-secondary-600 flex items-center px-3 py-2 justify-center gap-2">
               <Image src={icons.googleIcon} alt="login" className="w-6" />
               <p className="supportText_medium text-white">Login</p>
             </button>
